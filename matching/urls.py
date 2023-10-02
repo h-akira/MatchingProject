70d32cc577ade486732ba686466c14ef84322125
h-akira <79441679+h-akira@users.noreply.github.com>
2023-10-02 12:15:25 +0900

    fix (add urls)

---
+++ matching/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import 
+
+app_name = "matching"
+
+urlpatterns = [
+  # path('',index, name='index'),
+  # path('delete/<int:id>/', delete, name='delete'),
+]
