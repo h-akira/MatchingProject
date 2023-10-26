0ad36405dc9d5f63eba6c3f7a89912362b2db515
h-akira <79441679+h-akira@users.noreply.github.com>
2023-10-26 12:19:32 +0900

    fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,13 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
-class CustomUserForm(forms.ModelForm):
+class CustomUserCreationForm(UserCreationForm):
+  class Meta:
+    model = CustomUser
+    fields = ('username', 'password1', 'password2')
+
+class CustomUserEditForm(forms.ModelForm):
   class Meta:
     model = CustomUser
     fields = ("last_name", "first_name", "registration_type","profile")
